92336de419481ea834087bd2a0a05020939ca982
timdrive1 <timdrive2013@gmail.com>
2017-05-23 18:32:27 +0300

    Initial commit

---
+++ src/ChocolateBoiler.java
@@ -0,0 +1,45 @@
+/**
+ * Created by Тим on 23.05.2017.
+ */
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+
+    private static ChocolateBoiler uniqueInstance;
+
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public static ChocolateBoiler getUniqueInstance(){
+        if(uniqueInstance == null){
+            uniqueInstance = new ChocolateBoiler();
+        }
+        return uniqueInstance;
+    }
+
+    public void fill(){
+        if(isEmpty()){
+            empty = false;
+            boiled = false;
+        }
+    }
+    public void drain(){
+        if(!isEmpty()&& isBoiled()){
+            empty = true;
+        }
+    }
+    public void boil(){
+        if(!isEmpty()&&!isBoiled()){
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty(){
+        return empty;
+    }
+    public boolean isBoiled(){
+        return boiled;
+    }
+}
